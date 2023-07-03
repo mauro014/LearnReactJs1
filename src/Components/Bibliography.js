43a9ae725f8e0e032c83b299188d66d4bfd3946c
Mauricio <mauro014@gmail.com>
2023-07-03 11:36:12 -0500

    Adding some links

--- src/Components/Bibliography.js
+++ src/Components/Bibliography.js
@@ -5,8 +5,10 @@ function Bibliography(){
         <div>
             <h3>Bibliography</h3>
             <ul>
+                <li><a href="https://github.com/nodesource/distributions">Installing Node on Ubuntu 22.05</a></li>
                 <li><a href="https://www.freecodecamp.org/news/get-started-with-react-for-beginners/">How to Get Started With React – A Beginner's Guide</a></li>
                 <li><a href="https://www.youtube.com/watch?v=DLX62G4lc44">Learn React JS - Full Course for Beginners - Tutorial 2019</a></li>
+                <li><a href="https://code.visualstudio.com/docs/sourcecontrol/intro-to-git">Use git in VS Code</a></li>
             </ul>
         </div>
     );
